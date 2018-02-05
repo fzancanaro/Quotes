2b808571738c198cf3e70e7b98c503c044fd58e0
Flavia <zancanaro.flavia@gmail.com>
2018-02-05 12:01:23 +1100

    fix

---
+++ data/quotes.ts
@@ -0,0 +1,71 @@
+export default [
+  {
+    category: 'inspirational',
+    quotes: [
+      {
+        id: '1',
+        person: 'Theodore Roosevelt',
+        text: 'Believe you can and you\'re halfway there'
+      },
+      {
+        id: '2',
+        person: 'Norman Vincent Peale',
+        text: 'Change your thoughts and you change your world.'
+      },
+      {
+        id: '3',
+        person: 'Robert H. Schuller',
+        text: 'What great thing would you attempt if you knew you could not fail?'
+      }
+    ],
+    icon: 'brush'
+  },
+  {
+    category: 'ability',
+    quotes: [
+      {
+        id: '4',
+        person: 'John Wooden',
+        text: 'Ability may get you to the top, but it takes character to keep you there.'
+      },
+      {
+        id: '5',
+        person: 'Robert Frost',
+        text: 'Education is the ability to listen to almost anything without losing your temper.'
+      }
+    ],
+    icon: 'bicycle'
+  },
+  {
+    category: 'philosophy',
+    quotes: [
+      {
+        id: '6',
+        person: 'Albert Einstein',
+        text: 'Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe.'
+      },
+      {
+        id: '7',
+        person: 'Mahatma Gandhi',
+        text: 'Be the change that you wish to see in the world.'
+      }
+    ],
+    icon: 'ion-android-bulb'
+  },
+  {
+    category: 'motivational',
+    quotes: [
+      {
+        id: '8',
+        person: 'Jim Rohn',
+        text: 'Either you run the day or the day runs you.'
+      },
+      {
+        id: '9',
+        person: 'Donna Brazile',
+        text: 'I was motivated to be different in part because I was different.'
+      }
+    ],
+    icon: 'body'
+  }
+];
